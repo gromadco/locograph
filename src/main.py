3203c139549800653e6bf64131682b55c0c7108b
Artem Dudarev <dudarev@gmail.com>
2015-07-16 16:43:37 +0300

    User can send data

--- src/main.py
+++ src/main.py
@@ -1,11 +1,21 @@
 """`main` is the top level module for your Flask application."""
 
+import datetime
 
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
+
+from google.appengine.ext import db
+
+
+class User(db.Model):
+    email = db.EmailProperty(required=True)
+    places = db.ListProperty(db.Link)
+    subscribed_at = db.DateTimeProperty()
 
 
 app = Flask(__name__)
+
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
@@ -14,6 +24,39 @@ app = Flask(__name__)
 def hello(name=None):
     """Return index.html. """
     return render_template('index.html')
+
+
+@app.route('/subscribe', methods=['POST'])
+def subscribe():
+
+    links = []
+    for i in range(1, 5):
+        l = request.form.get('place{}'.format(i), None)
+        if l:
+            links.append(db.Link(l))
+
+    u = User(
+        email=request.form['email'],
+        places=links)
+    u.suscribed_at = datetime.datetime.now()
+    u.put()
+
+    return "Thank you for subscribing!"
+
+
+@app.route('/users')
+def users():
+
+    q = User.all()
+    res = ""
+    for user in q:
+        res += "<strong>{}</strong><br/>".format(user.email)
+        for p in user.places:
+            res += "<ul>"
+            res += '<li><a href="{}">{}</a></li>'.format(p, p)
+            res += "</ul>"
+
+    return res
 
 
 @app.errorhandler(404)
